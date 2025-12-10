839a5657f5ad46f6d1b01631ddafc1b67d784b18
Vijaya Lakshmi Yarramsetti <vijaya123laxmi143@gmail.com>
2025-12-10 19:51:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Implement Set in Python/implement-set-in-python.py
+++ Difficulty: Easy/Implement Set in Python/implement-set-in-python.py
@@ -1,13 +1,19 @@
-#User function Template for python3
+# User function Template for python3
 
 # Function to insert
 # no return should be there, and no print statement
 def insert(s, element):
     s.add(element)
-    
+
+
+# Function to remove element from set
+# No return and nothing to print
 def remove_from_set(s, element):
-    s.remove(element)
-  
+    # discard() avoids error if element not present
+    s.discard(element)
+
+
+# Function to find sum of elements in set
+# return value should be there as sum
 def sum_set(s):
     return sum(s)
-    
